b4018a09aebc902d2169310276f791fb381103d8
jinghaochua <jinghao_chua@hotmail.com>
2026-08-21 01:37:54 +0800

    Fix bug

--- src/main/java/Bastion.java
+++ src/main/java/Bastion.java
@@ -1,15 +1,16 @@
 public class Bastion {
     public static void main(String[] args) {
         String banner = 
-        " ___             _   _               \n"
-        "| _ )   __ _ ___| |_(_) ___  _ __    \n"
-        "| __ \ / _` / __| __| |/ _ \| '_ \   \n"
-        "||__| | (_| \__ \ |_| | (_) | | | |  \n"
-        "|____/ \__,_|___/\__|_|\___/|_| |_|  \n";
+        " ___             _   _               \n" +
+        "| _ )   __ _ ___| |_(_) ___  _ __    \n" +
+        "| __ \\ / _` / __| __| |/ _ \\| '_ \\   \n" +
+        "||__| | (_| \\__ \\ |_| | (_) | | | |  \n" +
+        "|____/ \\__,_|___/\\__|_|\\___/|_| |_|  \n";
+
         
         System.out.println("____________________________________________________________");
         System.out.println(banner);
-        System.out.println("Hello! I'm [CHATBOT NAME].");
+        System.out.println("Hello! I'm Bastion.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
