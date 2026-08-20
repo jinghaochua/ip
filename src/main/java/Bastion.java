c5ea6960b475c4ca62d91ffeb3d16d71229d3f95
jinghaochua <jinghao_chua@hotmail.com>
2026-08-21 01:27:22 +0800

    Change name to Bastion

---
+++ src/main/java/Bastion.java
@@ -0,0 +1,11 @@
+public class Bastion {
+    public static void main(String[] args) {
+        String banner = 
+        " ___             _   _               \n"
+        "| _ )   __ _ ___| |_(_) ___  _ __    \n"
+        "| __ \ / _` / __| __| |/ _ \| '_ \   \n"
+        "||__| | (_| \__ \ |_| | (_) | | | |  \n"
+        "|____/ \__,_|___/\__|_|\___/|_| |_|  \n";
+        System.out.println(banner);
+    }
+}
