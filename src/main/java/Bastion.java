0becfcd3b56497175457310ea2ffecfd6f47fc37
jinghaochua <jinghao_chua@hotmail.com>
2026-09-02 16:30:58 +0800

    add task.java

--- src/main/java/Bastion.java
+++ src/main/java/Bastion.java
@@ -4,37 +4,6 @@ public class Bastion {
     /** The maximum number of tasks the application keeps. */
     private static final int MAX_TASKS = 100;
 
-    static class Task {
-        String description;
-        boolean isDone;
-
-        Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        void markAsDone() {
-            this.isDone = true;
-        }
-
-        void markAsNotDone() {
-            this.isDone = false;
-        }
-
-        String getStatusIcon() {
-            return (isDone ? "X" : " "); // Returns X if done, space if not
-        }
-    }
-
-    /**
-     * Starts the task list application and processes commands until the user enters
-     * {@code bye}.
-     * Any input other than those commands is added as a task; {@code list} displays
-     * stored tasks.
-     *
-     * @param args command-line arguments, which are not used
-     */
-
     public static void main(String[] args) {
         Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
@@ -50,7 +19,6 @@ public class Bastion {
 
                 if (input.equals("bye")) {
                     System.out.println("Bye. Hope to see you again soon!");
-                    printLine();
                     break;
                 }
 
@@ -60,7 +28,8 @@ public class Bastion {
                     try {
                         int taskNumber = Integer.parseInt(input.substring(5)) - 1;
                         tasks[taskNumber].markAsDone();
-                        
+                        printLine();
+
                         if (taskNumber >= 0 && taskNumber < taskCount) {
                             System.out.println("Nice! I've marked this task as done:");
                             System.out.println("  [" + tasks[taskNumber].getStatusIcon() + "] " + tasks[taskNumber].description);
@@ -74,6 +43,7 @@ public class Bastion {
                     try {
                         int taskNumber = Integer.parseInt(input.substring(7)) - 1;
                         tasks[taskNumber].markAsNotDone();
+                        printLine();
                         if (taskNumber >= 0 && taskNumber < taskCount) {
                             System.out.println("OK, I've marked this task as not done yet:");
                             System.out.println("  [" + tasks[taskNumber].getStatusIcon() + "] " + tasks[taskNumber].description);
@@ -88,10 +58,10 @@ public class Bastion {
                 } else {
                     tasks[taskCount] = new Task(input);
                     taskCount++;
+                    printLine();
                     System.out.println("added: " + input);
                 }
 
-                printLine();
             }
         }
     }
@@ -104,11 +74,12 @@ public class Bastion {
 
         for (int index = 0; index < taskCount; index++) {
             System.out.println((index + 1) + ". [" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
+
         }
     }
 
-    /** Prints the divider used to separate program input and output. */
     private static void printLine() {
         System.out.println("____________________________________________________________");
     }
+    
 }
